9979abebe397c9732477628ddc0101c0f249a6da
Olivia Kesler <oakesler@protonmail.ch>
2019-12-27 20:36:13 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -42,4 +42,26 @@ class TextAnalyzer
  
     biggest
   end
+  
+   def most_used_letter_count
+    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+ 
+    arr1.map do |c|
+      arr2[c] =  arr.count(c)
+    end
+ 
+    biggest = { arr2.keys.first => arr2.values.first }
+ 
+    arr2.each do |key, value|
+      if value > biggest.values.first
+        biggest = {}
+        biggest[key] = value
+      end
+    end
+ 
+    biggest[key]
+  end
 end
